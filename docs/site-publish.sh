4d3458fd50ba3175c2e07bea23a3fb9decc2f6c2
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-03-25 16:42:31 +0000

    documentation

--- docs/site-publish.sh
+++ docs/site-publish.sh
@@ -5,17 +5,12 @@ cd $DIR
 
 SOURCE_BRANCH="master"
 DEST_BRANCH="gh-pages"
-TMP_DIR="tmp"
 
 #git checkout $SOURCE_BRANCH
 jekyll build -d $DIR/docs/jekyll-site/ -s $DIR/docs/jekyll-bootstrap/
 git checkout $DEST_BRANCH
 # This will remove previous files, which we may not want (e.g. CNAME)
-# git rm -qr .
 cp -r $DIR/docs/jekyll-site/. .
-# Delete this script from the output
-#rm ./$SELF
-#rm -r $TMP_DIR
 git add -A
 git commit -m "Published updates"
 # May not want to push straight away
