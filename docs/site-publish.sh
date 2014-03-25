4fcc1bd63948bc32f7450e2ffd100cc15bbf073d
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-03-25 16:19:26 +0000

    documentation

--- docs/site-publish.sh
+++ docs/site-publish.sh
@@ -8,12 +8,12 @@ SOURCE_BRANCH="master"
 DEST_BRANCH="gh-pages"
 TMP_DIR="tmp"
 
-git checkout $SOURCE_BRANCH
+#git checkout $SOURCE_BRANCH
 jekyll build -d $DIR/jekyll-site/ -s $DIR/jekyll-bootstrap/
-git checkout $DEST_BRANCH
+#git checkout $DEST_BRANCH
 # This will remove previous files, which we may not want (e.g. CNAME)
 # git rm -qr .
-cp -r $DIR/jekyll-site/. .
+#cp -r $DIR/jekyll-site/. .
 # Delete this script from the output
 #rm ./$SELF
 #rm -r $TMP_DIR
