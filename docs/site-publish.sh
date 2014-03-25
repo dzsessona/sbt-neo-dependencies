eb8a795436798349d8ce2f55f0a34664c62d5e4a
Diego Zambelli Sessona <dzsessona@gilt.com>
2014-03-25 16:15:57 +0000

    cleaning up

--- docs/site-publish.sh
+++ docs/site-publish.sh
@@ -9,16 +9,16 @@ DEST_BRANCH="gh-pages"
 TMP_DIR="tmp"
 
 git checkout $SOURCE_BRANCH
-jekyll build -d $TMP_DIR -s $DIR/jekyll-bootstrap/
+jekyll build -d $DIR/jekyll-site/ -s $DIR/jekyll-bootstrap/
 git checkout $DEST_BRANCH
 # This will remove previous files, which we may not want (e.g. CNAME)
 # git rm -qr .
-cp -r $TMP_DIR/. .
+cp -r $DIR/jekyll-site/. .
 # Delete this script from the output
-rm ./$SELF
-rm -r $TMP_DIR
-git add -A
-git commit -m "Published updates"
+#rm ./$SELF
+#rm -r $TMP_DIR
+#git add -A
+#git commit -m "Published updates"
 # May not want to push straight away
-git push origin gh-pages
-git checkout $SOURCE_BRANCH
+#git push origin gh-pages
+#git checkout $SOURCE_BRANCH
